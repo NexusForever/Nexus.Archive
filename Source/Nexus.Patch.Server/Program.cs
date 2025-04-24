35b67f298caa6d4dbd4ef486188215ff1513a7fe
Jason Couture <jasonc@alertr.info>
2025-04-24 09:51:42 -0400

    feat: Convert IWebHostBuilder to WebApplicationBuilder

--- Source/Nexus.Patch.Server/Program.cs
+++ Source/Nexus.Patch.Server/Program.cs
@@ -1,14 +1,36 @@
-﻿using Microsoft.AspNetCore;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Nexus.Patch.Server.Services;
 
 namespace Nexus.Patch.Server;
 
 public class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
-        CreateWebHostBuilder(args).Build().Run();
+        var builder = CreateWebApplication(args);
+
+        builder.Services.AddControllers();
+        
+        builder.Services.AddActivatedSingleton<IGameDataFiles, GameDataFiles>();
+        
+        var app = builder.Build();
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        
+        app.UseRouting();
+        app.MapControllers();
+        await app.RunAsync();
     }
+    
+    public static WebApplicationBuilder CreateWebApplication(string[] args) 
+        => WebApplication.CreateBuilder(args);
 
     public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
         WebHost.CreateDefaultBuilder(args)
